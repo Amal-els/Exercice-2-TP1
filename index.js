6ba88cfe378828c5c5b7a85c14fa900a1b19ac15
Amal-els <amalbahri19@gmail.com>
2025-02-17 22:53:03 +0100

    latest commit

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-/*const size_picker = document.querySelector('#size');
-const font_picker =document.querySelector("#fonts");
-const color_picker = document.querySelector('#color');
-var t = document.querySelector(".text p");
-picker.addEventListener("input",function(){
-    t.style.color = picker.value;});
-size_picker.addEventListener("input",function(){
-    t.style.fontSize = size_picker.value + "px";});
-font_picker.addEventListener("input",function(){
-    t.style.fontFamily = font_picker.value;
-});*/
 
 function addEvent(p,cl){
     let t = document.querySelector(".text p");
