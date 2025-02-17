b8d11a0f74217d396abfa6d2de07cafd2546cd23
Amal-els <amalbahri19@gmail.com>
2025-02-17 14:57:05 +0100

    last commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const picker = document.querySelector('#color');
-const size_picker = document.querySelector('#size');
+/*const size_picker = document.querySelector('#size');
 const font_picker =document.querySelector("#fonts");
-
+const color_picker = document.querySelector('#color');
 var t = document.querySelector(".text p");
 picker.addEventListener("input",function(){
     t.style.color = picker.value;});
@@ -9,5 +8,27 @@ size_picker.addEventListener("input",function(){
     t.style.fontSize = size_picker.value + "px";});
 font_picker.addEventListener("input",function(){
     t.style.fontFamily = font_picker.value;
-});
+});*/
+
+function addEvent(p,cl){
+    let t = document.querySelector(".text p");
+    if (cl == 'color'){
+        t.style.color = p.value;
+    }
+    else if (cl == 'fonts'){
+        t.style.fontFamily = p.value;
+    }
+    else if (cl == 'size'){
+        t.style.fontSize = p.value + "px";
+    }
+}
+
+const size_picker = document.querySelector('#size');
+const font_picker =document.querySelector("#fonts");
+const color_picker = document.querySelector('#color');
+color_picker.addEventListener("input",()=>addEvent(color_picker,'color'));
+font_picker.addEventListener("input",()=>addEvent(font_picker,'fonts'));
+size_picker.addEventListener("input",()=>addEvent(size_picker,'size'))
+
+
 
